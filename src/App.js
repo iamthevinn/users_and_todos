1a13beaf857daf2d58ae46fc84a88784492fe47d
Vince Carini <vincecarini@northwesternmutual.com>
2018-02-05 12:32:14 -0600

    navigation works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
 import './ui-toolkit/css/nm-cx/main.css';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+
+const Home = (props) => (
+  <div className="homeScreen">
+    <div className="homeTextContainer">
+      Users<br />and<br />Todos
+    </div>
+  </div>
+)
+
+const Users = (props) => (
+  <div className="homeScreen">
+    <div className="homeTextContainer">
+      Users
+    </div>
+  </div>
+)
+
+const Todos = (props) => (
+  <div className="homeScreen">
+    <div className="homeTextContainer">
+      Todos
+    </div>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ul className="filter-nav">
-          <li className="filter-nav-entry">
-            <button>Home</button>
-          </li>
-          <li className="filter-nav-entry">
-            <button>Users</button>
-          </li>
-          <li className="filter-nav-entry">
-            <button>Todos</button>
-          </li>
-        </ul>
-        
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <ul className="filter-nav">
+            <li className="filter-nav-entry">
+            <button><Link to="/">Home</Link></button>
+            </li>
+            <li className="filter-nav-entry">
+              <button><Link to="/users">Users</Link></button>
+            </li>
+            <li className="filter-nav-entry">
+            <button><Link to="/todos">Todos</Link></button>
+            </li>
+          </ul>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/users" component={Users} />
+          <Route exact path="/todos" component={Todos} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
