0538935296da97e1c094505d04ba697bef0c496f
Vince Carini <vincecarini@northwesternmutual.com>
2018-02-06 11:46:04 -0600

    some nm style. need to fix underlined

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
-//import './ui-toolkit/css/nm-cx/main.css';
+import './ui-toolkit/css/nm-cx/main.css';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 
 const DESCRIPTION = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 const users = ["Vince", "Bruce", "Sam", "Joe"]
 const todos = ["Users and Todos", "Ghibli App", "Dojo Dossier", "Voting App II"]
+
+const NmTab = ({exact, to, tabName}) => {
+  return (
+    <Route exact={exact} path={to} children={({match}) => {
+      return (
+        <li className={`tab-title ${match ? 'active' : ''}`} ><Link to={to}>{tabName}</Link></li>
+      )}
+    } />
+  )
+}
+
+const ButtonGroup = props => (
+  <nav>
+    <ul className="tabs">
+      <NmTab exact={true} to={"/"} tabName="Home" />
+      <NmTab to={"/users"} tabName="Users" />
+      <NmTab to={"/todos"} tabName="Todos" />
+    </ul>
+  </nav>
+)
 
 const Home = (props) => (
   <div className="homeScreen">
@@ -73,30 +93,13 @@ const Todos = props => (
   </div>
 )
 
-const NavBar = props => (
-  <div>
-    <div className="navigation">
-      <li className="navItem">
-        <Link className={props.location.pathname === "/" ? "navItemContent clickedNav" : "navItemContent"} to="/">Home</Link>
-      </li>
-      <li className="navItem">
-        <Link className={props.location.pathname.startsWith("/users") ? "navItemContent clickedNav" : "navItemContent"} to="/users">Users</Link>
-      </li>
-      <li className="navItem">
-        <Link className={props.location.pathname.startsWith("/todos") ? "navItemContent clickedNav" : "navItemContent"} to="/todos">Todos</Link>
-      </li>
-    </div>
-    <Route exact path="/" component={Home} />
-    <Route path="/users" component={Users} />
-    <Route path="/todos" component={Todos} />
-  </div>
-)
-
-
 const App = props => (
   <BrowserRouter>
     <div className="App">
-      <Route path='/' component={NavBar} />
+      <ButtonGroup />
+      <Route exact path="/" component={Home} />
+      <Route path="/users" component={Users} />
+      <Route path="/todos" component={Todos} />
     </div>
   </BrowserRouter>
 );
