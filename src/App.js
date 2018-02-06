ef921eefa72075cc523e80e3efa17c6e1b5c69dc
Vince Carini <vincecarini@northwesternmutual.com>
2018-02-06 07:07:19 -0600

    Better spacing and remove aqua hover to match his wireframe

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 
 const DESCRIPTION = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-const users = ["User 1", "User 2", "User 3", "User 4"]
-const todos = ["Todo 1", "Todo 2", "Todo 3", "Todo 4"]
+const users = ["Vince", "Bruce", "Sam", "Joe"]
+const todos = ["Users and Todos", "Ghibli App", "Dojo Dossier", "Voting App II"]
 
 const Home = (props) => (
   <div className="homeScreen">
@@ -33,7 +33,7 @@ const Users = props => {
     <div className="userDisplay">
       <div className="listOfItems">
         <ul>
-          {users.map((user, index) => (<li key={index + user} className="lineStyle"><Link className={props.location.pathname.includes(user) ? "listItem selectedListItem" : "listItem"} to={"/users/" + user}>{user}</Link></li>))}
+          {users.map((user, index) => (<li key={index + user} className="lineStyle"><Link className={props.location.pathname === "/users/" + user ? "listItem selectedListItem" : "listItem"} to={"/users/" + user}>{user}</Link></li>))}
         </ul>
       </div>
       <Route path='/users/:name' component={User} />
@@ -59,7 +59,7 @@ const Todos = props => {
     <div className="userDisplay">
       <div className="listOfItems">
         <ul>
-          {todos.map((todo, index) => (<li key={index + todo} className="lineStyle"><Link className={props.location.pathname.includes(todo) ? "listItem selectedListItem" : "listItem"} to={"/todos/" + todo}>{todo}</Link></li>))}
+          {todos.map((todo, index) => (<li key={index + todo} className="lineStyle"><Link className={props.location.pathname === "/todos/" + todo ? "listItem selectedListItem" : "listItem"} to={"/todos/" + todo}>{todo}</Link></li>))}
         </ul>
       </div>
       <Route path='/todos/:todoItem' component={Todo} />
@@ -74,12 +74,12 @@ const NavBar = props => {
 
   if (props.location.pathname === "/") {
     homeStyle = "navItemContent clickedNav"
-  } else if (props.location.pathname.includes("/users")) {
+  } else if (props.location.pathname.startsWith("/users")) {
     usersStyle = "navItemContent clickedNav"
-  } else if (props.location.pathname.includes("/todos")) {
+  } else if (props.location.pathname.startsWith("/todos")) {
     todosStyle = "navItemContent clickedNav"
   }
-  
+
   return (
     <div className="navigation">
       <li className="navItem">
